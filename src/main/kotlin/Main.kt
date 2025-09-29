618fba1233319695e5354ee2e0cfdb494bc36d12
kirill <mrcreepipc@mail.ru>
2025-09-29 18:56:53 +0400

    KTB-02

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,9 @@ import java.io.File
 fun main() {
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
+    wordsFile.writeText("hello привет\n")
+    wordsFile.appendText("cat кошка\n")
+    wordsFile.appendText("dog собака\n")
     println(wordsFile.readLines().map { it + "\n" }.joinToString(""))
 }
 
