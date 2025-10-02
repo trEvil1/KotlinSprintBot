4145cae21eb173d3ea35adf46435557fe68f9193
kirill <mrcreepipc@mail.ru>
2025-10-02 09:37:26 +0400

    File parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,27 @@ import java.io.File
 fun main() {
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("cat кошка\n")
-    wordsFile.appendText("dog собака\n")
-    println(wordsFile.readLines().map { it + "\n" }.joinToString(""))
+    val dictionary = mutableListOf<Word>()
+    val lines: List<String> = wordsFile.readLines()
+    var answer = 0
+    var word = Word("", "", 0)
+
+    for (line in lines) {
+        val line = line.split("|")
+        word = word.copy(original = line[0], translate = line[1])
+        println(line[0] + " перевод")
+        val translate = readln()
+        if (translate == word.translate) {
+            word.correctAnswer = 1
+            answer += 1
+        } else word.correctAnswer = 0
+        dictionary.add(word)
+    }
+    println(dictionary)
 }
 
+data class Word(
+    val original: String,
+    val translate: String?,
+    var correctAnswer: Int
+)
