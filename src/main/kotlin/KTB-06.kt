40cf2bd2e367a7a40e064bbb8a7f3dbb5f2e8038
kirill <mrcreepipc@mail.ru>
2025-10-11 10:15:29 +0400

    Statistic

--- src/main/kotlin/KTB-06.kt
+++ src/main/kotlin/KTB-06.kt
@@ -1,19 +1,23 @@
 import java.io.File
 
+const val CORRECT_ANSWER_NUMBER = 3
 fun main() {
     val dictionary = loadDictionary()
-    val totalCount = dictionary.lastIndex + 1
+    val totalCount = dictionary.size
     while (true) {
         println("1 - Учить слова\n2 - Статистика\n0 - Выход")
         val input = readln()
         when (input.toInt()) {
             1 -> println("Учить слова")
-            2 ->
-            {
-                val learnedCount = (dictionary.filter { it.correctAnswerCount >= 3 }).lastIndex + 1
-                val percent = ((learnedCount.toDouble() / totalCount) * 100).toInt()
-                println("Выучено $learnedCount из $totalCount | $percent%\n")
+            2 -> {
+                if (totalCount != 0) {
+                    val learnedCount =
+                        dictionary.count { it.correctAnswerCount != null && it.correctAnswerCount!! >= 3 }
+                    val percent = ((learnedCount.toDouble() / totalCount) * 100).toInt()
+                    println("Выучено $learnedCount из $totalCount | $percent%\n")
+                } else println("Словарь пуст")
             }
+
             0 -> return
             else -> println("Введите число 1, 2 или 0")
         }
@@ -27,8 +31,9 @@ fun loadDictionary(): List<Word> {
 
     for (line in lines) {
         val parts = line.split("|")
-        val word = Word(original = parts[0], translate = parts[1], parts[2].toInt())
+        val word = Word(original = parts[0], translate = parts[1], parts[2].toIntOrNull())
         dictionary.add(word)
+        println(dictionary)
     }
     return dictionary
 }
@@ -36,5 +41,5 @@ fun loadDictionary(): List<Word> {
 data class Word(
     val original: String,
     val translate: String,
-    var correctAnswerCount: Int
+    var correctAnswerCount: Int?
 )
