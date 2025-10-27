43e0f0ae13d3fd2b8b4c3341bacfb222d4050088
kirill <mrcreepipc@mail.ru>
2025-10-27 11:17:18 +0400

    Send message method

--- src/main/kotlin/ru/trevil1/telegrambot/telegram/Telegram.kt
+++ src/main/kotlin/ru/trevil1/telegrambot/telegram/Telegram.kt
@@ -1,34 +1,27 @@
 package ru.trevil1.telegrambot.telegram
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-
-private const val TELEGRAM_API_URL = "https://api.telegram.org/bot"
-
 fun main(args: Array<String>) {
 
-    val botToken = args[0]
+    val services = TelegramBotService(args[0])
+
     var lastUpdateId = 0
+
     val lastUpdateIdRegex = "\"update_id\":\\s*(\\d+),".toRegex()
     val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
+    val chatIdRegex = ",\"chat\":\\{\"id\":(.+?),".toRegex()
 
     while (true) {
-        val updates: String = getUpdates(botToken, lastUpdateId + 1)
+
+        val updates: String = services.getUpdates(lastUpdateId + 1)
         Thread.sleep(2000)
         lastUpdateId = lastUpdateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
+        val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
         val text = messageTextRegex.find(updates)?.groups?.get(1)?.value
+
+        services.sendMessage(chatId, updates)
+
+        println(updates)
         println(lastUpdateId)
         println(text ?: "")
     }
-}
-
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdate = "$TELEGRAM_API_URL$botToken/getUpdates?offset=$updateId"
-    val client = HttpClient.newBuilder().build()
-    val requestUpdate = HttpRequest.newBuilder().uri(URI.create(urlGetUpdate)).build()
-    val responseUpdate = client.send(requestUpdate, HttpResponse.BodyHandlers.ofString())
-
-    return responseUpdate.body()
 }
---
+++ src/main/kotlin/ru/trevil1/telegrambot/telegram/TgBotService.kt
@@ -0,0 +1,34 @@
+package ru.trevil1.telegrambot.telegram
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+private const val TELEGRAM_API_URL = "https://api.telegram.org/bot"
+
+class TelegramBotService(val botToken: String) {
+    fun getUpdates(updateId: Int): String {
+        val urlGetUpdate = "$TELEGRAM_API_URL$botToken/getUpdates?offset=$updateId"
+        val client = HttpClient.newBuilder().build()
+        val requestUpdate = HttpRequest.newBuilder().uri(URI.create(urlGetUpdate)).build()
+        val responseUpdate = client.send(requestUpdate, HttpResponse.BodyHandlers.ofString())
+
+        return responseUpdate.body()
+    }
+
+    fun sendMessage(chatId: Int, update: String): String {
+        val hello = ",\"text\":\"Hello\""
+        if (hello in update) {
+            val text = "Hello"
+            val urlSendMessage = "$TELEGRAM_API_URL$botToken/sendMessage?chat_id=$chatId&text=$text"
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
+            val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+            return response.body()
+        } else {
+            return ""
+        }
+    }
+}
